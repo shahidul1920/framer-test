51a218df0b64e495beba1cdd676af36b39505a24
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-03-08 14:22:53 +0600

    cool todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import Animation0 from './components/Animation0'
 import Animation1 from './components/Animation1'
 
 
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <div className='bg-slate-900 min-h-[1000px]'>
-      <Animation1 />
+      <Animation0 />
     </div>
   )
 }
