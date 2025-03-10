db407510426a384a95c84049b6e67ec482c5acf8
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-03-10 23:08:35 +0600

    useInview

--- src/components/AnimationView.jsx
+++ src/components/AnimationView.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 export default function AnimationView() {
-  return (
-    <div className='bg-slate-700 text-white p-10 h-screen grid place-items-center'>
-      <motion.div initial={{x:-400, opacity:0}} whileInView={{x:0, opacity:1}} transition={{duration:0.2}} className='size-30 bg-stone-100 rounded-lg'>
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef);
+  const animeControls = useAnimation()
 
-      </motion.div>
+  useEffect(() => {
+    if (isInView) {
+      animeControls.start('show')
+    }
+  }, [isInView]);
+
+  return (
+    <div className="bg-slate-700 text-white p-10 h-screen grid place-items-center">
+      <motion.div
+        ref={containerRef}
+        initial="hidden"
+        variants={{ hidden: { opacity: 0, x:-100 }, show: { opacity: 1, x:0 }} }
+        animate={animeControls}
+        transition={{ duration: 1, ease: "easeInOut", delay: 2 }}
+        className="size-30 bg-stone-100 rounded-lg"
+      ></motion.div>
     </div>
-  )
+  );
 }
